4f4db52ab411570da0871ffdf8ba2abc951ac1b0
dizhongdi <2755063993@qq.com>
2022-07-12 14:18:35 +0800

    微信支付

--- pages/course/_id.vue
+++ pages/course/_id.vue
@@ -1,81 +1,74 @@
 <template>
   <div id="aCoursesList" class="bg-fa of">
-    <!-- 课程详情 开始 -->
+    <!-- /课程详情 开始 -->
     <section class="container">
-<!-- 课程所属分类 开始 -->
-<section class="path-wrap txtOf hLh30">
-    <a href="#" title class="c-999 fsize14">首页</a>
-    \
-    <a href="/course" title class="c-999 fsize14">课程列表</a>
-    \
-    <span class="c-333 fsize14">{{ course.subjectLevelOne }}</span>
-    \
-    <span class="c-333 fsize14">{{ course.subjectLevelTwo }}</span>
-</section>
-<!-- /课程所属分类 结束 -->
-
-<!-- 课程基本信息 开始 -->
-<div>
-    <article class="c-v-pic-wrap" style="height: 357px;">
-        <section id="videoPlay" class="p-h-video-box">
-            <img :src="course.cover" :alt="course.title" class="dis c-v-pic">
-        </section>
-    </article>
-    <aside class="c-attr-wrap">
-        <section class="ml20 mr15">
+      <section class="path-wrap txtOf hLh30">
+        <a href="#" title class="c-999 fsize14">首页</a>
+        \
+        <a href="#" title class="c-999 fsize14">{{courseWebVo.subjectLevelOne}}</a>
+        \
+        <span class="c-333 fsize14">{{courseWebVo.subjectLevelTwo}}</span>
+      </section>
+      <div>
+        <article class="c-v-pic-wrap" style="height: 357px;">
+          <section class="p-h-video-box" id="videoPlay">
+            <img height="357px" :src="courseWebVo.cover" :alt="courseWebVo.title" class="dis c-v-pic">
+          </section>
+        </article>
+        <aside class="c-attr-wrap">
+          <section class="ml20 mr15">
             <h2 class="hLh30 txtOf mt15">
-                <span class="c-fff fsize24">{{ course.title }}</span>
+              <span class="c-fff fsize24">{{courseWebVo.title}}</span>
             </h2>
             <section class="c-attr-jg">
-                <span class="c-fff">价格：</span>
-                <b class="c-yellow" style="font-size:24px;">￥{{ course.price }}</b>
+              <span class="c-fff">价格：</span>
+              <b class="c-yellow" style="font-size:24px;">￥{{courseWebVo.price}}</b>
             </section>
             <section class="c-attr-mt c-attr-undis">
-                <span class="c-fff fsize14">主讲： {{ course.teacherName }}&nbsp;&nbsp;&nbsp;</span>
+              <span class="c-fff fsize14">主讲： {{courseWebVo.teacherName}}&nbsp;&nbsp;&nbsp;</span>
             </section>
             <section class="c-attr-mt of">
-                <span class="ml10 vam">
-                    <em class="icon18 scIcon"/>
-                    <a class="c-fff vam" title="收藏" href="#" >收藏</a>
-                </span>
+              <span class="ml10 vam">
+                <em class="icon18 scIcon"></em>
+                <a class="c-fff vam" title="收藏" href="#" >收藏</a>
+              </span>
             </section>
             <section class="c-attr-mt">
-                <a href="#" title="立即观看" class="comm-btn c-btn-3">立即观看</a>
+              <a @click="createOrders()" href="#" title="立即购买" class="comm-btn c-btn-3">立即购买</a>
             </section>
-        </section>
-    </aside>
-    <aside class="thr-attr-box">
-        <ol class="thr-attr-ol clearfix">
+          </section>
+        </aside>
+        <aside class="thr-attr-box">
+          <ol class="thr-attr-ol">
             <li>
-                <p>&nbsp;</p>
-                <aside>
-                    <span class="c-fff f-fM">购买数</span>
-                    <br>
-                    <h6 class="c-fff f-fM mt10">{{ course.buyCount }}</h6>
-                </aside>
+              <p>&nbsp;</p>
+              <aside>
+                <span class="c-fff f-fM">购买数</span>
+                <br>
+                <h6 class="c-fff f-fM mt10">{{courseWebVo.buyCount}}</h6>
+              </aside>
             </li>
             <li>
-                <p>&nbsp;</p>
-                <aside>
-                    <span class="c-fff f-fM">课时数</span>
-                    <br>
-                    <h6 class="c-fff f-fM mt10">{{ course.lessonNum }}</h6>
-                </aside>
+              <p>&nbsp;</p>
+              <aside>
+                <span class="c-fff f-fM">课时数</span>
+                <br>
+                <h6 class="c-fff f-fM mt10">20</h6>
+              </aside>
             </li>
             <li>
-                <p>&nbsp;</p>
-                <aside>
-                    <span class="c-fff f-fM">浏览数</span>
-                    <br>
-                    <h6 class="c-fff f-fM mt10">{{ course.viewCount }}</h6>
-                </aside>
+              <p>&nbsp;</p>
+              <aside>
+                <span class="c-fff f-fM">浏览数</span>
+                <br>
+                <h6 class="c-fff f-fM mt10">501</h6>
+              </aside>
             </li>
-        </ol>
-    </aside>
-    <div class="clear"/>
-</div>
-<!-- /课程基本信息 结束 -->
-
+          </ol>
+        </aside>
+        <div class="clear"></div>
+      </div>
+      <!-- /课程封面介绍 -->
       <div class="mt20 c-infor-box">
         <article class="fl col-7">
           <section class="mr30">
@@ -86,100 +79,111 @@
                 </section>
               </div>
               <article class="ml10 mr10 pt20">
+                <div>
+                  <h6 class="c-i-content c-infor-title">
+                    <span>课程介绍</span>
+                  </h6>
+                  <div class="course-txt-body-wrap">
+                    <section class="course-txt-body">
+                      <p v-html="courseWebVo.description">{{courseWebVo.description}}</p>
+                    </section>
+                  </div>
+                </div>
+                <!-- /课程介绍 -->
+                <div class="mt50">
+                  <h6 class="c-g-content c-infor-title">
+                    <span>课程大纲</span>
+                  </h6>
+                  <section class="mt20">
+                    <div class="lh-menu-wrap">
+                      <menu id="lh-menu" class="lh-menu mt10 mr10">
+                        <ul>
+                          <!-- 文件目录 -->
+                          <li class="lh-menu-stair" v-for="chapter in chapterVideoList" :key="chapter.id">
+                            <a href="javascript: void(0)" :title="chapter.title" class="current-1">
+                              <em class="lh-menu-i-1 icon18 mr10"></em>{{chapter.title}}
+                            </a>
 
-<!-- 课程详情介绍 开始 -->
-<div>
-    <h6 class="c-i-content c-infor-title">
-        <span>课程介绍</span>
-    </h6>
-    <div class="course-txt-body-wrap">
-        <section class="course-txt-body">
-            <!-- 将内容中的html翻译过来 -->
-            <p v-html="course.description">{{ course.description }}</p>
-        </section>
-    </div>
-</div>
-<!-- /课程详情介绍 结束 -->
-<!-- 课程大纲 开始-->
-<div class="mt50">
-    <h6 class="c-g-content c-infor-title">
-        <span>课程大纲</span>
-    </h6>
-    <section class="mt20">
-        <div class="lh-menu-wrap">
-            <menu id="lh-menu" class="lh-menu mt10 mr10">
-                <ul>
-                    <!-- 课程章节目录 -->
-                    <li v-for="chapter in chapterList" :key="chapter.id" class="lh-menu-stair">
-                        <a :title="chapter.title" href="javascript: void(0)" class="current-1">
-                            <em class="lh-menu-i-1 icon18 mr10"/>{{ chapter.title }}
-                        </a>
-                        <ol class="lh-menu-ol" style="display: block;">
-                            <li v-for="video in chapter.children" :key="video.id" class="lh-menu-second ml30">
-                                <a :href="'/player/'+video.videoSourceId" :title="video.title" target="_blank">
-                                    <span v-if="video.free === true" class="fr">    
-                                        <i class="free-icon vam mr10">免费试听</i>
-                                    </span>
-                                    <em class="lh-menu-i-2 icon16 mr5">&nbsp;</em>{{ video.title }}
+                            <ol class="lh-menu-ol" style="display: block;">
+                              <li class="lh-menu-second ml30" v-for="video in chapter.children" :key="video.id">
+                                <a :href="'/player/'+video.videoSourceId" target="_blank">
+                                  <span class="fr">
+                                    <i class="free-icon vam mr10">免费试听</i>
+                                  </span>
+                                  <em class="lh-menu-i-2 icon16 mr5">&nbsp;</em>{{video.title}}
                                 </a>
-                            </li>
-                        </ol>
-                    </li>
-                </ul>
-            </menu>
-        </div>
-    </section>
-    </div>
-    <!-- /课程大纲 结束 -->
+                              </li>
+                              
+                            </ol>
+
+                          </li>
+                        </ul>
+                      </menu>
+                    </div>
+                  </section>
+                </div>
+                <!-- /课程大纲 -->
               </article>
             </div>
           </section>
         </article>
         <aside class="fl col-3">
           <div class="i-box">
-<!-- 主讲讲师 开始-->
-<div>
-    <section class="c-infor-tabTitle c-tab-title">
-        <a title href="javascript:void(0)">主讲讲师</a>
-    </section>
-    <section class="stud-act-list">
-        <ul style="height: auto;">
-            <li>
-                <div class="u-face">
-                    <a :href="'/teacher/'+course.teacherId" target="_blank">
-                        <img :src="course.avatar" width="50" height="50" alt>
-                    </a>
-                </div>
-                <section class="hLh30 txtOf">
-                    <a :href="'/teacher/'+course.teacherId" class="c-333 fsize16 fl" target="_blank">{{ course.teacherName }}</a>
-                </section>
-                <section class="hLh20 txtOf">
-                    <span class="c-999">{{ course.intro }}</span>
-                </section>
-            </li>
-        </ul>
-    </section>
-</div>
-<!-- /主讲讲师 结束 -->
+            <div>
+              <section class="c-infor-tabTitle c-tab-title">
+                <a title href="javascript:void(0)">主讲讲师</a>
+              </section>
+              <section class="stud-act-list">
+                <ul style="height: auto;">
+                  <li>
+                    <div class="u-face">
+                      <a href="#">
+                        <img :src="courseWebVo.avatar" width="50" height="50" alt>
+                      </a>
+                    </div>
+                    <section class="hLh30 txtOf">
+                      <a class="c-333 fsize16 fl" href="#">{{courseWebVo.teacherName}}</a>
+                    </section>
+                    <section class="hLh20 txtOf">
+                      <span class="c-999">{{courseWebVo.intro}}</span>
+                    </section>
+                  </li>
+                </ul>
+              </section>
+            </div>
           </div>
         </aside>
-        <div class="clear"/>
+        <div class="clear"></div>
       </div>
     </section>
     <!-- /课程详情 结束 -->
   </div>
 </template>
+
 <script>
-import course from "@/api/course"
+import courseApi from '@/api/course'
+import ordersApi from '@/api/order'
 export default {
-  asyncData({ params, error }) {
-    return course.getById(params.id).then(response => {
-      console.log(response);
-      return { 
-        course: response.data.data.course,
-        chapterList: response.data.data.chapterVoList
-      }
-    })
-  }
-}
+   asyncData({ params, error }) {
+     return courseApi.getById(params.id)
+        .then(response => {
+          return {
+            courseWebVo: response.data.data.course,
+            chapterVideoList: response.data.data.chapterVoList,
+            courseId:params.id
+          }
+        })
+   },
+   methods:{
+     //生成订单
+     createOrders() {
+       ordersApi.createOrder(this.courseId)
+        .then(response => {
+          //获取返回订单号
+          //生成订单之后，跳转订单显示页面
+          this.$router.push({path:'/orders/'+response.data.data.orderId})
+        })
+     }
+   }
+};
 </script>
